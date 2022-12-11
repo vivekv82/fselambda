78b178c22db1bd97c827c9c6ba650fde8fd4ebf1
Vivek Vidyashankar <Vivek.Vidyashankar@bcbsma.com>
2022-12-11 20:25:59 +0530

    correcting classpath

--- src/main/java/skilltracker/fse/SkillsTrackerApp.java
+++ src/main/java/skilltracker/fse/SkillsTrackerApp.java
@@ -4,16 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
-
-import skilltracker.fse.controller.SkillsController;
-//https://blogs.ashrithgn.com/spring-boot-rest-api-deployed-in-aws-lambda-serverless/
 
 // https://github.com/galovics/aws-lambda-spring-boot2/blob/master/springboot2/src/main/java/com/arnoldgalovics/blog/BookController.java
 @SpringBootApplication
 @Configuration
 @EnableAutoConfiguration
-@Import({SkillsController.class})
 public class SkillsTrackerApp {
     
     public static void main(String[] args) {
